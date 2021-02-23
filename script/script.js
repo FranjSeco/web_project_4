4bbeefe259d0614415cb3651337891a1dc9d1183
frankii -- <franjseco@gmail.com>
2021-02-23 19:40:49 +0100

    fix: unify event listeners on card create

--- script/script.js
+++ script/script.js
@@ -1,21 +1,76 @@
 //VARIABLES
 const overlay = document.querySelector("#overlay");
+const closeIcon = overlay.querySelector(".close-icon");
+const imagePopupModal = overlay.querySelector(".image-popup");
+const imageFormModal = overlay.querySelector(".image-form");
+const profileFormModal = overlay.querySelector(".edit-form");
 
 const editBtn = document.querySelector(".profile__info-btn");
 const addBtn = document.querySelector(".profile__add-btn");
+const nameText = document.querySelector(".profile__info-title");
+const aboutText = document.querySelector(".profile__info-about");
 
 const cards = document.querySelector("#el-template").content;
-const element = cards.querySelector(".element");
-const elementSection = document.querySelector(".elements");
 
-const imagePopup = overlay.querySelector(".image-popup");
-const formPopup = overlay.querySelector(".edit-form");
-const imageForm = overlay.querySelector(".image-form");
+const elementSection = document.querySelector(".elements");
+const formImageName = imageFormModal.querySelector(".image-form__input_name");
+const formImageLink = imageFormModal.querySelector(".image-form__input_about");
+
+const formProfileName = profileFormModal.querySelector(".edit-form__input_name");
+const formProfileAbout = profileFormModal.querySelector(".edit-form__input_about");
+const formProfileBtn = profileFormModal.querySelector(".edit-form__btn")
+
+const imagePic = imagePopupModal.querySelector(".image-popup__picture");
+const imageFig = imagePopupModal.querySelector(".image-popup__caption");
+
+
 
 const addPlace = document.querySelector(".profile__add-btn");
 const profileEdit = document.querySelector(".profile__info-btn");
-const modal = overlay.querySelector(".image-form");
-const modalProfile = overlay.querySelector(".edit-form");
+const formImageSubmit = overlay.querySelector(".image-form__form");
+const formProfileSubmit = overlay.querySelector(".edit-form__form");
+
+// FUNCTIONS
+function cardCreation(card) {
+  const cardElement = cards.querySelector(".element");
+  const cloneCard = cardElement.cloneNode(true);
+
+  const cardImage = cloneCard.querySelector(".element__image");
+  const cardName = cloneCard.querySelector(".element__title");
+  const cardLike = cloneCard.querySelector("element__like-btn");
+
+  return cloneCard;
+}
+
+function overlayOn() {
+  overlay.classList.add("overlay_popup");
+}
+
+function overlayOff() {
+  overlay.classList.remove("overlay_popup");
+}
+
+function imageOn() {
+  imagePopupModal.classList.add("image-popup_on");
+}
+
+function imageOff() {
+  imagePopupModal.classList.remove("image-popup_on");
+}
+
+function editFormOn() {
+  profileFormModal.classList.add("edit-form_on");
+}
+function editFormOff() {
+  profileFormModal.classList.remove("edit-form_on");
+}
+
+function imageFormOn() {
+  imageFormModal.classList.add("image-form_on");
+}
+function imageFormOff() {
+  imageFormModal.classList.remove("image-form_on");
+}
 
 // CARD TEMPLATE
 const initialCards = [
@@ -45,111 +100,98 @@ const initialCards = [
   }
 ];
 
-initialCards.forEach(item => {
-  const cloneCard = element.cloneNode(true);
-  cloneCard.querySelector(".element__image").src = item.link;
-  cloneCard.querySelector(".element__image").alt = `Picture of ${item.name}`;
-  cloneCard.querySelector(".element__title").textContent = item.name;
-  elementSection.append(cloneCard);
+initialCards.forEach(card => {
+  const cardItems = cardCreation(card);
+  elementSection.append(cardItems);
+  cardItems.querySelector(".element__image").src = card.link;
+  cardItems.querySelector(".element__image").alt = `Picture of ${card.name}`;
+  cardItems.querySelector(".element__title").textContent = card.name;
+
 });
-
-// FUNCTIONS
-function overlayToggle() {
-  if (overlay.classList.contains("overlay_popup")) {
-    overlay.classList.remove("overlay_popup");
-  }
-  console.log("HEY")
-}
-
-function imageToggle() {
-  if (imagePopup.classList.contains("image-popup_on")) {
-    imagePopup.classList.remove("image-popup_on");
-  }
-}
-
-function formToggle() {
-  if (formPopup.classList.contains("edit-form_on")) {
-    formPopup.classList.remove("edit-form_on");
-  }
-}
-
-function imageFormToggle() {
-  if (imageForm.classList.contains("image-form_on")) {
-    imageForm.classList.remove("image-form_on");
-  }
-}
 
 // CLOSE ICON
 overlay.addEventListener("click", function (event) {
   if (event.target.classList.contains("close-icon")) {
-    overlayToggle()
-    imageToggle()
-    formToggle()
-    imageFormToggle()
+    overlayOff();
+    imageOff();
+    editFormOff();
+    imageFormOff();
   }
 });
 
 // PROFILE: NAME AND PROFESSION
+
+
 profileEdit.addEventListener("click", function () {
-  const nameText = document.querySelector(".profile__info-title");
-  const aboutText = document.querySelector(".profile__info-about");
-  overlay.classList.add("overlay_popup");
-  formPopup.classList.add("edit-form_on");
-  modalProfile.querySelector(".edit-form__input_name").value = nameText.textContent;
-  modalProfile.querySelector(".edit-form__input_about").value = aboutText.textContent;
-  modalProfile.querySelector(".edit-form__btn").addEventListener("click", function (evt) {
-    evt.preventDefault();
-    if (modalProfile.querySelector(".edit-form__input_name").value == "" || modalProfile.querySelector(".edit-form__input_about").value == "") {
-      alert("Please, insert valid information.")
-    } else {
-      nameText.textContent = modalProfile.querySelector(".edit-form__input_name").value;
-      aboutText.textContent = modalProfile.querySelector(".edit-form__input_about").value;
-      overlay.classList.remove("overlay_popup");
-      formPopup.classList.remove("edit-form_on");
-    }
-  });
+  overlayOn();
+  editFormOn();
+  formProfileName.value = nameText.textContent;
+  formProfileAbout.value = aboutText.textContent;
+});
+
+
+formProfileSubmit.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  nameText.textContent = formProfileName.value;
+  aboutText.textContent = formProfileAbout.value;
+  overlayOff();
+  editFormOff();
+});
+
+profileFormModal.addEventListener("click", function (event) {
+  if (event.target.classList.contains("close-icon")) {
+    overlayOff();
+    editFormOff();
+  }
 });
 
 // ADDING CARD
 addPlace.addEventListener("click", function () {
-  overlay.classList.add("overlay_popup");
-  imageForm.classList.add("image-form_on");
-  modal.querySelector(".image-form__input_name").value = "";
-  modal.querySelector(".image-form__input_about").value = "";
-  modal.querySelector(".image-form__input_name").placeholder = "Title";
-  modal.querySelector(".image-form__input_about").placeholder = "Image URL";
-  modal.querySelector(".image-form__btn").addEventListener("click", function (evt) {
-    evt.preventDefault();
-    if (modal.querySelector(".image-form__input_about").value !== "" && modal.querySelector(".image-form__input_name").value !== "") {
-      overlay.classList.remove("overlay_popup");
-      imageForm.classList.remove("image-form_on");
-      const newCard = element.cloneNode(true);
-      elementSection.prepend(newCard);
-      evt.stopImmediatePropagation();
-      newCard.querySelector(".element__image").src = modal.querySelector(".image-form__input_about").value;
-      newCard.querySelector(".element__image").alt = `Picture of ${modal.querySelector(".image-form__input_name").value}`;
-      newCard.querySelector(".element__title").textContent = modal.querySelector(".image-form__input_name").value;
-    } else {
-      alert("Please, insert valid information.")
-    }
-  });
+  overlayOn();
+  imageFormOn();
+  formImageName.value = "";
+  formImageLink.value = "";
 });
+
+
+formImageSubmit.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  overlayOff();
+  imageFormOff();
+  const newCard = cardCreation();
+  elementSection.prepend(newCard);
+  evt.stopImmediatePropagation();
+  newCard.querySelector(".element__image").src = formImageLink.value;
+  newCard.querySelector(".element__image").alt = `Picture of ${formImageName.value}`;
+  newCard.querySelector(".element__title").textContent = formImageName.value;
+});
+
+imageFormModal.addEventListener("click", function (event) {
+  if (event.target.classList.contains("close-icon")) {
+    overlayOff();
+    imageFormOff();
+  }
+});
+
 
 // PICTURE POPUP
 elementSection.addEventListener("click", function (event) {
   if (event.target.classList.contains("element__image")) {
-    overlay.classList.add("overlay_popup");
-    const imageOn = overlay.querySelector(".image-popup").classList.toggle("image-popup_on");
-    const imagePic = overlay.querySelector(".image-popup__picture");
-    const imageFig = overlay.querySelector(".image-popup__caption");
+    overlayOn();
+    imageOn();
+
     imagePic.src = event.target.src;
     imagePic.alt = event.target.alt;
     imageFig.textContent = event.target.alt;
   }
 });
 
-
-
+imagePopupModal.addEventListener("click", function (event) {
+  if (event.target.classList.contains("close-icon")) {
+    overlayOff();
+    imageOff();
+  }
+});
 
 
 // LIKE BTN
