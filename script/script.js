bf649ec632925c7f9556aabde0a9df58ff8b5f2b
frankii -- <franjseco@gmail.com>
2021-02-24 13:27:15 +0100

    fix: cardMaker function

--- script/script.js
+++ script/script.js
@@ -7,13 +7,11 @@ const imagePopupModal = overlay.querySelector(".image-popup");
 const imageFormModal = overlay.querySelector(".image-form");
 const profileFormModal = overlay.querySelector(".edit-form");
 
+
 const editBtn = document.querySelector(".profile__info-btn");
 const addBtn = document.querySelector(".profile__add-btn");
 const nameText = document.querySelector(".profile__info-title");
 const aboutText = document.querySelector(".profile__info-about");
-
-
-
 
 const formImageName = imageFormModal.querySelector(".image-form__input_name");
 const formImageLink = imageFormModal.querySelector(".image-form__input_about");
@@ -35,19 +33,17 @@ const closeProfileForm = profileFormModal.querySelector(".close-icon");
 const closeImageForm = imageFormModal.querySelector(".close-icon");
 
 
+
 // FUNCTIONS
 function cardMaker(card) {
   const cardElement = cardTemplate.querySelector(".element");
   const cloneCard = cardElement.cloneNode(true);
   const cardDeleteButton = cloneCard.querySelector('.element__trash');
+
   const cardImage = cloneCard.querySelector(".element__image");
   const cardName = cloneCard.querySelector(".element__title");
   const cardLike = cloneCard.querySelector(".element__like-btn");
 
-  // DELETE BUTTON
-  cardDeleteButton.addEventListener("click", function (event) {
-    event.target.closest(".element").remove(cloneCard);
-  });
 
   // LIKE BUTTON
   cardLike.addEventListener("click", function (event) {
@@ -56,50 +52,26 @@ function cardMaker(card) {
 
   // PICTURE POPUP
   cardImage.addEventListener("click", function (event) {
-    overlayOn();
-    imageOn();
+    toggleModalWindow(imagePopupModal);
     imagePic.src = event.target.src;
     imagePic.alt = event.target.alt;
     imageFig.textContent = event.target.alt;
   });
 
-  closeImage.addEventListener("click", function () {
-    overlayOff();
-    imageOff();
+  // DELETE BUTTON
+  cardDeleteButton.addEventListener("click", function (event) {
+    event.target.closest(".element").remove(cloneCard);
   });
+
 
   return cloneCard;
 }
 
-function overlayOn() {
-  overlay.classList.add("overlay_popup");
+function toggleModalWindow(modalWindow) {
+  modalWindow.classList.toggle("popup_opened");
+  overlay.classList.toggle("overlay_popup");
 }
 
-function overlayOff() {
-  overlay.classList.remove("overlay_popup");
-}
-
-function imageOn() {
-  imagePopupModal.classList.add("image-popup_on");
-}
-
-function imageOff() {
-  imagePopupModal.classList.remove("image-popup_on");
-}
-
-function editFormOn() {
-  profileFormModal.classList.add("edit-form_on");
-}
-function editFormOff() {
-  profileFormModal.classList.remove("edit-form_on");
-}
-
-function imageFormOn() {
-  imageFormModal.classList.add("image-form_on");
-}
-function imageFormOff() {
-  imageFormModal.classList.remove("image-form_on");
-}
 
 // CARD TEMPLATE
 const initialCards = [
@@ -132,49 +104,47 @@ const initialCards = [
 initialCards.forEach(card => {
   const cardItems = cardMaker(card);
   cardWrapper.append(cardItems);
+
   cardItems.querySelector(".element__image").src = card.link;
   cardItems.querySelector(".element__image").alt = `Picture of ${card.name}`;
   cardItems.querySelector(".element__title").textContent = card.name;
+});
 
+
+// CLOSE IMAGE
+closeImage.addEventListener("click", function () {
+  toggleModalWindow(imagePopupModal);
 });
 
 
 // PROFILE: NAME AND PROFESSION
 profileEdit.addEventListener("click", function () {
-  overlayOn();
-  editFormOn();
+  toggleModalWindow(profileFormModal);
   formProfileName.value = nameText.textContent;
   formProfileAbout.value = aboutText.textContent;
 });
-
-
 
 formProfileSubmit.addEventListener("submit", function (evt) {
   evt.preventDefault();
   nameText.textContent = formProfileName.value;
   aboutText.textContent = formProfileAbout.value;
-  overlayOff();
-  editFormOff();
+  toggleModalWindow(profileFormModal);
 });
 
-
 closeProfileForm.addEventListener("click", function (event) {
-  overlayOff();
-  editFormOff();
+  toggleModalWindow(profileFormModal);
 });
 
 // ADDING CARD
 addPlace.addEventListener("click", function () {
-  overlayOn();
-  imageFormOn();
+  toggleModalWindow(imageFormModal);
   formImageName.value = "";
   formImageLink.value = "";
 });
 
 formImageSubmit.addEventListener("submit", function (evt) {
   evt.preventDefault();
-  overlayOff();
-  imageFormOff();
+  toggleModalWindow(imageFormModal);
   const newCard = cardMaker();
   cardWrapper.prepend(newCard);
   evt.stopImmediatePropagation();
@@ -183,15 +153,6 @@ formImageSubmit.addEventListener("submit", function (evt) {
   newCard.querySelector(".element__title").textContent = formImageName.value;
 });
 
-
 closeImageForm.addEventListener("click", function (event) {
-  if (event.target.classList.contains("close-icon")) {
-    overlayOff();
-    imageFormOff();
-  }
+  toggleModalWindow(imageFormModal);
 });
-
-
-
-
-
